0120deeab29f80dfdca93a45d36c646533a0f1cf
augieafr <augieafriyansyah@gmail.com>
2025-02-12 09:41:41 +0700

    create AlbumsService.js

---
+++ src/services/posgres/AlbumsService.js
@@ -0,0 +1,47 @@
+const { Pool } = require("pg");
+const { nanoid } = require("nanoid");
+const { executeQuery } = require("../../utils/dbHelper");
+const InvariantError = require("../../exceptions/client/InvariantError");
+const { generalMapDbToModel } = require("../../utils/mapper");
+const NotFoundError = require("../../exceptions/client/NotFoundError");
+
+class AlbumsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addAlbum({ name, year }) {
+    const id = `album-${nanoid(16)}`;
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const query = {
+      text: 'INSERT INTO albums VALUES($1, $2, $3, $4, $5) RETURNING id',
+      values: [id, name, year, createdAt, updatedAt]
+    };
+
+    const result = await executeQuery(this._pool, query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError("Album gagal ditambahkan")
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getAlbumsById(id) {
+    const query = {
+      text: 'SELECT * FROM albums WHERE id = $1',
+      values: [id]
+    };
+
+    const result = await executeQuery(this._pool, query)
+
+    if (!result.rows.length) {
+      throw new NotFoundError("Album tidak ditemukan");
+    }
+    return result.rows.map(generalMapDbToModel);
+  }
+}
+
+module.exports = AlbumsService;
